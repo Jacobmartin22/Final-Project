83a69f0eff3bdbf022773d6df6950671241d2f8d
Jacob Kell Martin <119345707+Jacobmartin22@users.noreply.github.com>
2023-04-22 12:42:03 -0500

    Add files via upload

---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../index.css";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount = () => {};
+
+
+  render() {
+    // console.log(this.state);
+
+    
+    return (
+      <div className="border">
+       HomePage
+       
+      </div>
+    );
+  }
+}
+
+export default Home;
